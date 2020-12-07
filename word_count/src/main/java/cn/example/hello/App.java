1fda8fcc541474008c8697bb2bb48af31236e93b
王智 <z.wang@tianrang-inc.com>
2020-12-07 15:11:37 +0800

    init sub project wordcount

---
+++ word_count/src/main/java/cn/example/hello/App.java
@@ -0,0 +1,9 @@
+package cn.example.hello;
+
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(String.format("hello world in java: %s", new Date()));
+    }
+}
